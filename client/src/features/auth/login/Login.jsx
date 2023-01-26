c0dc22d154d4e72b20c21d22abdf3faab4b335bb
Michael <maroc.michael@hotmail.com>
2023-01-27 00:38:20 +0100

    correction d'un bug empêchant le reset du h1 lors d'un remplissage d'input sur les pages login et register

--- client/src/features/auth/login/Login.jsx
+++ client/src/features/auth/login/Login.jsx
@@ -4,19 +4,14 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../authSlice";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
 import "./login.scss";
 import { emailCheck, passwordCheck } from "../../../common/utils/Regex"; 
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const [errMsg, setErrMsg] = useState("");
-  const resetErrMsg = () => setErrMsg("");
-
-  const { register, handleSubmit, reset, formState: { errors }} = useForm();
   const [login] = useLoginMutation();
+  const { register, handleSubmit, reset, formState: { errors }} = useForm();
 
   const onSubmit = async (data) => {
     try {
@@ -24,18 +19,14 @@ const Login = () => {
         dispatch(setCredentials({ token: result.accessToken }));
         reset();
         navigate("/home");
-    } catch (err) {
-      if (!err?.originalStatus) setErrMsg("No Server Response");
-      else if (err.originalStatus === 400) return setErrMsg("All fields are required");
-      else if (err.originalStatus === 401) return setErrMsg("Unauthorized");
-      else setErrMsg("Login Failed");
+    } catch (err) { 
+      console.log(err) 
     }
   };
 
   const content = (
     <section className="login">
       <form onSubmit={handleSubmit(onSubmit)}>
-        {errMsg && <p className="errMsg form-error">{errMsg}</p>}
         <h1>Login</h1>
 
         <div className="form-group">
@@ -45,7 +36,6 @@ const Login = () => {
               required: 'email is required', 
               pattern: { value: emailCheck, message: "please enter a valid email" }
             })}
-            onChange={resetErrMsg}
             autoComplete="off"
           />
           {errors.email?.message && 
@@ -60,10 +50,9 @@ const Login = () => {
               required: 'password is required', 
               pattern: { value: passwordCheck, message: "please enter a valid password" }
             })}
-            onChange={resetErrMsg}
             autoComplete="off"
           />
-          {errors.password?.message &&
+          {errors.password?.message && 
             <p className="errMsg" aria-label="assertive">{errors.password?.message}</p>
           }
         </div>
--- client/src/features/auth/register/Register.jsx
+++ client/src/features/auth/register/Register.jsx
@@ -1,36 +1,27 @@
 import { useSignUpMutation } from "../authApiSlice";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
 import "./register.scss";
 import { usernameCheck, emailCheck, passwordCheck } from "../../../common/utils/Regex"; 
 
 const Register = () => {
   const navigate = useNavigate();
-
-  const [errMsg, setErrMsg] = useState("");
-  const resetErrMsg = () => setErrMsg("");
-
-  const { register, handleSubmit, reset,formState: { errors }} = useForm();
   const [signUp] = useSignUpMutation();
+  const { register, handleSubmit, reset,formState: { errors }} = useForm();
 
   const onSubmit = async (data) => {
     try {
       await signUp(data).unwrap();
       reset();
       navigate("/");
-    } catch (err) {
-      if (!err?.originalStatus) setErrMsg("No Server Response");
-      else if (err.originalStatus === 400) setErrMsg("All fields are required");
-      else if (err.originalStatus === 409) setErrMsg("You can't use this email");
-      else setErrMsg("Login Failed");
+    } catch (err) { 
+      console.log(err) 
     }
   };
 
   const content = (
     <section className="register">
       <form onSubmit={handleSubmit(onSubmit)}>
-        {errMsg && <p className="errMsg form-error">{errMsg}</p>}
         <h1>Register</h1>
 
         <div className="form-group">
@@ -40,7 +31,6 @@ const Register = () => {
               required: 'username is required', 
               pattern: { value: usernameCheck, message: "please enter a valid username" }
             })}
-            onChange={resetErrMsg}
             autoComplete="off"
           />
           {errors.username?.message && 
@@ -55,7 +45,6 @@ const Register = () => {
               required: 'email is required', 
               pattern: { value: emailCheck, message: "please enter a valid email" }
             })}
-            onChange={resetErrMsg}
             autoComplete="off"
           />
           {errors.email?.message && 
@@ -70,7 +59,6 @@ const Register = () => {
               required: 'password is required', 
               pattern: { value: passwordCheck, message: "please enter a valid password" } 
             })}
-            onChange={resetErrMsg}
             autoComplete="off"
           />
           {errors.password?.message && 
