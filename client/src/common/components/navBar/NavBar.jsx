09d9c74bb83c2a1f1b9d6f27457bbc453eb3f602
Michael <maroc.michael@hotmail.com>
2023-02-05 02:51:44 +0100

    ajout du logo et du responsive

--- client/src/common/components/navBar/NavBar.jsx
+++ client/src/common/components/navBar/NavBar.jsx
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHome, faFileImage, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useSendLogoutMutation } from "../../../features/auth/authApiSlice";
 import { getCurrentToken } from "../../../features/auth/authSlice";
+import Logo from "/images/icon-left-font-monochrome-white.svg";
 import "./navBar.scss";
 
 const NavBar = () => {
@@ -9,12 +12,25 @@ const NavBar = () => {
   const token = useSelector(getCurrentToken);
 
   return token ? (
-    <nav className="home-nav">
-      <h1>Beta test</h1>
+    <nav className="nav">
+      <img className="nav-logo" src={Logo} alt="Logo" />
       <ul>
-        <Link to="/home">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link onClick={sendLogout}>Logout</Link>
+        <li className="nav-full-size"><Link to="/home">Home</Link></li>
+        <li className="nav-full-size"><Link to="/profile">Profile</Link></li>
+        <li className="nav-full-size"><Link onClick={sendLogout}>Logout</Link></li>
+
+        <li className="nav-small-size">
+          <p>Home</p>
+          <Link to="/home"><FontAwesomeIcon icon={faHome} /></Link>
+        </li>
+        <li className="nav-small-size">
+          <p>Profile</p>
+          <Link to="/profile"><FontAwesomeIcon icon={faFileImage} /></Link>
+        </li>
+        <li className="nav-small-size">
+          <p>Logout</p>
+          <Link onClick={sendLogout}><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
+        </li>
       </ul>
     </nav>
   ) : null;
